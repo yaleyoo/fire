5d3037ab91fa2e5b533ec974594d13589d5990e8
Qi Yuan <qyuan4@student.unimelb.edu.au>
2017-08-15 23:04:19 +1000

    general project

---
+++ fire-web/src/main/java/com/fes/web/interceptor/AppLoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.fes.web.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by qigege on 2017/8/14.
+ */
+public class AppLoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}

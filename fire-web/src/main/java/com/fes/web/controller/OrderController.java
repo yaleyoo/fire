fdd63c27832f713e8b545eb31da3b0b5634facd0
Qi Yuan <qyuan4@student.unimelb.edu.au>
2017-10-04 23:46:13 +1100

    add user injection resolver

--- fire-web/src/main/java/com/fes/web/controller/OrderController.java
+++ fire-web/src/main/java/com/fes/web/controller/OrderController.java
@@ -1,7 +1,14 @@
 package com.fes.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by qigege on 2017/10/2.
  */
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
 }
